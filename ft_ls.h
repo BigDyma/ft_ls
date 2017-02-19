a66fc0691fdcfed581b833590a27a72336e401c6
BigDyma <d1mastrelet@gmail.com>
2017-02-19 16:36:37 +0200

    nono

--- ft_ls.h
+++ ft_ls.h
@@ -1,14 +1,15 @@
- /* ************************************************************************** */
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_ls.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dstrelet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/12/28 16:56:49 by dstrelet          #+#    #+#             */
-/*   Updated: 2017/02/09 13:17:00 by dstrelet         ###   ########.fr       */
+/*   Created: 2017/02/17 17:26:52 by dstrelet          #+#    #+#             */
+/*   Updated: 2017/02/17 17:26:57 by dstrelet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #ifndef FT_LS_H
 
 # define FT_LS_H
@@ -26,105 +27,89 @@
 # include <string.h>
 # include <unistd.h>
 # include <errno.h>
+# include <sys/acl.h>
+# include <sys/xattr.h>
 
-# define RED        "\x1b[31m"
-# define GREEN      "\x1b[32m"
-# define YELLOW     "\x1b[33m"
-# define BLUE       "\x1b[34m"
-# define MAGENTA    "\x1b[35m"
-# define CYAN       "\x1b[36m"
-# define RESET      "\x1b[0m"
-# define WHITE      "\033[0m"
-
-typedef struct           flag_lis
+typedef struct		s_flag_lis
 {
-     int l;
-     int r_upper;
-     int a;
-     int r;
-     int t;
-     int u;
-     int f;
-     int g;
-     int d;
-     int m;
-     int g_upper;
-     int p;
-}                       flag_list;
-typedef struct lungimi
+	int			l;
+	int			r_upper;
+	int			a;
+	int			r;
+	int			t;
+	int			u;
+	int			f;
+	int			g;
+	int			d;
+	int			m;
+	int			g_upper;
+	int			p;
+}					t_flag_list;
+
+typedef struct		s_t_lungimi
 {
-    //int permis;
-    int nlink;
-    int pw_name;
-    int gr_name;
-    int size;
-    int timp;
-    int minor;
-    int major;
-    // int name;
-} lungimi;
-typedef struct      d_lst
+	int	nlink;
+	int	pw_name;
+	int	gr_name;
+	int	size;
+	int	timp;
+	int	minor;
+	int	major;
+}					t_lungimi;
+
+typedef struct		s_d_lst
 {
-    char    *name;
-    char    *path;
-    char    *parent;
-    time_t  date;
-    mode_t  st_mode;
-    nlink_t nlink;
-    uid_t   uid;
-    gid_t   gid;
-    off_t   size;
-    quad_t  blocks;
-    dev_t   st_rdev;
-    char    *timp;
-    struct  passwd  *pw;
-    struct  group   *gr;
-    char    *permis;
-    char *linkof;
-    struct  d_lst  *next;
-    int32_t         major;
-    int32_t         minor;
-}                        s_list;
+	char			*name;
+	char			*path;
+	char			*parent;
+	time_t			date;
+	time_t			udate;
+	int				i_am_from_the_dark_side;
+	mode_t			st_mode;
+	nlink_t			nlink;
+	uid_t			uid;
+	gid_t			gid;
+	off_t			size;
+	quad_t			blocks;
+	dev_t			st_rdev;
+	char			*timp;
+	struct passwd	*pw;
+	struct group	*gr;
+	char			*permis;
+	char			*linkof;
+	int				p;
+	char			acl;
+	struct s_d_lst	*next;
+	int32_t			major;
+	int32_t			minor;
+}					t_s_list;
 
-// PROTOTIPURI
-void print_ll(s_list *headd);
-void print_name_by_list(s_list *temp);
-void insert(struct dirent *d, char *path, char *str);
-s_list *SortedMerge(s_list *a, s_list *b);
-s_list *SortedMerget(s_list *a, s_list *b);
-void FrontBackSplit(s_list *source,
-        s_list **frontRef, s_list **backRef);
-int ls(char *str);
-void    print_l();
-void print_name();
-void error(char *str);
-void reverse(s_list **head_ref);
-void searchflag(char *str);
-int     flag(char *str);
-void fuckof(char str);
-int in(char *str, char str2);
-int minmin(char *str);
-void MergeSort(s_list **headRef);
-void    display_file(char *str);
-lungimi antiponturi();
-    
-//GLOBALE
+void				print_ll(t_s_list *headd);
+void				print_name_by_list(t_s_list *temp);
+void				insert(struct dirent *d, char *path, char *str);
+t_s_list			*sortedmerge(t_s_list *a, t_s_list *b);
+// t_s_list			*sortedmerget(t_s_list *a, t_s_list *b);
+void				frontbacksplit(t_s_list *source,
+	t_s_list **frontref, t_s_list **backref);
+void				print_l();
+void				print_name();
+char				*permis(struct stat *elem);
+void				recurs(t_s_list *temp);
+void				error(char *str);
+void				reverse(t_s_list **head_ref);
+void				searchflag(char *str);
+int					flag(char *str);
+void				stop(char str);
+int					in(char *str, char str2);
+int					minmin(char *str);
+void				mine_mergesort(t_s_list **headref);
+void				display_file(char *str);
+void				filtreaza_path(char *str);
+t_lungimi			antiponturi();
+void				listdir(char *path);
 
-flag_list   flaguri;
-s_list      *head;
-int  g_p;
-int g_flagprove;
-int g_minmaj;
-
+t_flag_list			g_flaguri;
+int					g_flagprove;
+int					g_minmaj;
 
 #endif
-
-
-
-
-
-
-
-
-//  printf("%-*s%-*s%-*d%-*s%s\n", lungime.pw_name, temp->pw->pw_name, lungime.gr_name, temp->gr->gr_name, lungime.size, (int)temp->size, lungime.timp, temp->timp, temp->name);
-
